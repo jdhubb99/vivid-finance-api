2056a9df9590e752f3bce9f6b8aebb3516386c4b
Jake Hubbard <jacobdhubbard@gmail.com>
2025-04-13 14:45:36 -0400

    initial commit

---
+++ src/main/kotlin/app/vividfinance/vivid_finance_api/VividFinanceApiApplication.kt
@@ -0,0 +1,11 @@
+package app.vividfinance.vivid_finance_api
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class VividFinanceApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<VividFinanceApiApplication>(*args)
+}
---
+++ src/test/kotlin/app/vividfinance/vivid_finance_api/VividFinanceApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package app.vividfinance.vivid_finance_api
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class VividFinanceApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
